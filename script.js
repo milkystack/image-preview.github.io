f4ec6f58624d0a1c46e68ed787b090a785505a50
milkystack <103772943+milkystack@users.noreply.github.com>
2022-04-16 03:25:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ var params = new URLSearchParams(url.search);
 
 for(let param of params){
     if (param[0] != 'base'){
-        document.body.innerText += "https://media.discordapp.net/attachments/" + params.get('base') + param[1]; 
+        document.body.innerText += "https://media.discordapp.net/attachments/" + params.get('base') + param[1] + "\n"; 
     }
 }
