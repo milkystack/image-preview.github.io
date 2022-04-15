ca87296d0e952a252cd19eedb89daffacdc440a6
milkystack <103772943+milkystack@users.noreply.github.com>
2022-04-16 07:05:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,4 +15,4 @@ urls.forEach(function(val){
    document.body.innerHTML += "<img src='" + val + "' class='image'>";
 });
 
-document.body.innerHTML += "<div id='footer'>len: " + urls.length + "</div>";
+document.body.innerHTML += "<div id='footer'><div id='footer_content'>len: " + urls.length + "</div></div>";
