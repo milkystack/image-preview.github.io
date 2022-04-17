fa5ddce3028d8a9348e3d56cb6a42d97013df3ff
url-property2 <103869150+url-property2@users.noreply.github.com>
2022-04-17 12:50:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 
 var url = new URL(location.href);
 var params = new URLSearchParams(url.search);
-urls = []
+params.sort()
 
 for(let param of params){
     if (param[0] != 'base' && param[0] != 'waifu2x' && param[0] != 'bgc' && param[0] != 'anim' && param[0] != 'shortlink' && param[0] != 'title' && param[0] != 'link'){
         url = "https://media.discordapp.net/attachments/" + params.get('base') + param[1];
-        urls.push(url); 
+        document.body.innerHTML += "<img src='" + url + "' class='image'>";
     }
 }
-
-urls = urls.sort()
-urls.forEach(function(val){
-   document.body.innerHTML += "<img src='" + val + "' class='image'>";
-});
 
 document.body.innerHTML += "<div id='footer'><div class='footer_content'>画像は長押しで保存できます。</div></div>";
 document.body.innerHTML += "<div id='footer'><div class='footer_content'>[mode] waifu2x: " + params.get('waifu2x') + "　bgc: " + params.get('bgc') + "　anim: " + params.get('anim') + "</div></div>";
