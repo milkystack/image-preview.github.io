a074a71af622579ca05cf8aee85a67068dc897b8
milkystack <103772943+milkystack@users.noreply.github.com>
2022-04-17 12:05:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ urls.forEach(function(val){
 document.body.innerHTML += "<div id='footer'><div class='footer_content'>画像は長押しで保存できます。</div></div>";
 document.body.innerHTML += "<div id='footer'><div class='footer_content'>[mode] waifu2x: " + params.get('waifu2x') + "　bgc: " + params.get('bgc') + "　anim: " + params.get('anim') + "</div></div>";
 document.body.innerHTML += "<div id='footer'><div class='footer_content'>[len] " + urls.length + "</div></div>";
-document.body.innerHTML += "<div id='footer'> <div class='footer_content'>[stickerlink] <a href='" + params.get('link') + "'>" + params.get('title') + "</a></div></div>";
-document.body.innerHTML += "<div id='footer'> <div class='footer_content'>[sherelink] <a href='" + params.get('shortlink') + "'>" + params.get('shortlink') + "</a></div></div>";
+document.body.innerHTML += "<div id='footer'><div class='footer_content'>[stickerlink] <a href='" + params.get('link') + "'>" + params.get('title') + "</a></div></div>";
+document.body.innerHTML += "<div id='footer'><div class='footer_content'>[sherelink] <a href='" + params.get('shortlink') + "'>" + params.get('shortlink') + "</a></div></div>";
 
 // きゃーみないで
