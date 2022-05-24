8307386800b023598a896e842289e41807fbbfec
milkystack <103772943+milkystack@users.noreply.github.com>
2022-05-25 05:45:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function loads(){
         if (param[0] != 'base' && param[0] != 'waifu2x' && param[0] != 'bgc' && param[0] != 'anim' && param[0] != 'shortlink' && param[0] != 'title' && param[0] != 'link'){
             url = "https://media.discordapp.net/attachments/" + params.get('base') + param[1];
             if ( url.indexOf('.mp4') != -1) {
-                document.body.innerHTML += "<video src='" + url + "#t=0.001' controls loop playsinline preload='none' class='image'>";
+                document.body.innerHTML += "<video src='" + url + "#t=0.001' controls loop playsinline preload='metadata' class='image'>";
             }else{
                 document.body.innerHTML += "<img src='" + url + "' class='image'>";
             }
@@ -18,13 +18,13 @@ function loads(){
     }
     
     document.body.innerHTML += "<div id='footer'><div class='footer_content'>画像は長押しで保存できます。</div></div>";
-document.body.innerHTML += "<div id='footer'><div class='footer_content'>[mode] waifu2x: " + params.get('waifu2x') + "　bgc: " + params.get('bgc') + "　anim: " + params.get('anim') + "</div></div>";
-document.body.innerHTML += "<div id='footer'><div class='footer_content'>[content-len] " + count + "</div></div>";
-document.body.innerHTML += "<div id='footer'><div class='footer_content'>[sticker-link] <a href='" + params.get('link') + "'>" + params.get('title') + "</a></div></div>";
-document.body.innerHTML += "<div id='footer'><div class='footer_content'>[shere-link] <a href='" + params.get('shortlink') + "'>" + params.get('shortlink') + "</a></div></div>";
+    document.body.innerHTML += "<div id='footer'><div class='footer_content'>[mode] waifu2x: " + params.get('waifu2x') + "　bgc: " + params.get('bgc') + "　anim: " + params.get('anim') + "</div></div>";
+    document.body.innerHTML += "<div id='footer'><div class='footer_content'>[content-len] " + count + "</div></div>";
+    document.body.innerHTML += "<div id='footer'><div class='footer_content'>[sticker-link] <a href='" + params.get('link') + "'>" + params.get('title') + "</a></div></div>";
+    document.body.innerHTML += "<div id='footer'><div class='footer_content'>[shere-link] <a href='" + params.get('shortlink') + "'>" + params.get('shortlink') + "</a></div></div>";
 
 }
-setTimeout(loads, 1000);
+setTimeout(loads, 100);
 
 
 
