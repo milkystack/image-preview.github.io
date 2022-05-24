1b1a764e968edd626a27b9b04198f8c147b51019
milkystack <103772943+milkystack@users.noreply.github.com>
2022-05-24 22:02:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function loads(){
         }
     }
 }
-loads();
+setTimeout(loads, 3000);
 
 document.body.innerHTML += "<div id='footer'><div class='footer_content'>画像は長押しで保存できます。</div></div>";
 document.body.innerHTML += "<div id='footer'><div class='footer_content'>[mode] waifu2x: " + params.get('waifu2x') + "　bgc: " + params.get('bgc') + "　anim: " + params.get('anim') + "</div></div>";
